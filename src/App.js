265f0026815ae3a02f9402b9bd27439f84d67e54
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-15 15:57:09 -0400

    installed axios, set apod link in variable, created state for likes on both pics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import axios from 'axios'
@@ -6,7 +6,15 @@ import axios from 'axios'
 // API KEY l3mUrJBO7c7KEWvmQf1M4NSn3QjytWBij9jC0Oqd
 // EXAMPLE URL https://api.nasa.gov/planetary/apod?api_key=l3mUrJBO7c7KEWvmQf1M4NSn3QjytWBij9jC0Oqd
 
+// APOD API https://api.nasa.gov/planetary/apod?api_key=l3mUrJBO7c7KEWvmQf1M4NSn3QjytWBij9jC0Oqd
+
+const apodUrl = "https://api.nasa.gov/planetary/apod?api_key=l3mUrJBO7c7KEWvmQf1M4NSn3QjytWBij9jC0Oqd"
+
 function App() {
+
+  const [apodLikes, setApodLikes] = useState(0)
+  const [otherLikes, setOtherLikes] = useState(0)
+
   return (
     <div className="App">
       <p>
@@ -23,16 +31,26 @@ export default App;
 // General Structure
 
 // app
-    // header
-      // nav
-        // home
-        // github
-        // contact
-      // h1
-    // Photo of the day
-    // Another space photo
-    // Footer
-      // home
-      // github
-      // contact
+    // <header>
+      // <h1>SPACE</h1>
+      // <nav>
+        // <a href="#">Home</a>
+        // <a href="#">Github</a>
+        // <a href="#">Contact</a>
+      // </nav>
+    // </header>
+    // <div class="daily-container">
+      // <h2>Astronomy Picture of the Day</h2>
+      // <img src="#" alt="astronomy photo of the day">
+      // <button onClick=+1>Like</button>
+    // </div>
+    // <div class="random-pic-container">
+      // <h3>One more for ya!</h3>
+      // <img src="#" alt="random space">
+      // <button onClick=+1>Like</button>
+    // </div>
+    // <footer>
+      // <a href="#">Home</a>
+      // <a href="#">Github</a>
+      // <a href="#">Contact</a>
     
