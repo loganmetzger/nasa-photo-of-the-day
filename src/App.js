7c3af8361e96c16002957d4b3750dda45d0803c1
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-15 14:46:59 -0400

    check api key for nasa photo of the day and created a general structure outline

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./App.css";
 
+import axios from 'axios'
+
+// API KEY l3mUrJBO7c7KEWvmQf1M4NSn3QjytWBij9jC0Oqd
+// EXAMPLE URL https://api.nasa.gov/planetary/apod?api_key=l3mUrJBO7c7KEWvmQf1M4NSn3QjytWBij9jC0Oqd
+
 function App() {
   return (
     <div className="App">
@@ -13,3 +18,21 @@ function App() {
 }
 
 export default App;
+
+
+// General Structure
+
+// app
+    // header
+      // nav
+        // home
+        // github
+        // contact
+      // h1
+    // Photo of the day
+    // Another space photo
+    // Footer
+      // home
+      // github
+      // contact
+    
